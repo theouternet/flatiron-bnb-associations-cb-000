ea83510aa528e57366cc83eef10fdb422664f842
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-29 06:32:12 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :reviews 
-  has_many :listings
+  has_many :listings, :foreign_key => 'host_id'
+  has_many :reservations, through: :listings, :foreign_key => 'host_id'
 
-  
+  has_many :reviews, :foreign_key => 'guest_id'
+  has_many :trips, :class_name => :Reservation, :foreign_key => 'guest_id'
 end
